24849543ee52eaf6936ffad9eedb31c9f5bf672a
DiogoAMV <diogo.amv19@gmail.com>
2022-06-29 19:01:31 -0300

    :tada: Project finished

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Diogo',
+    },
+    {
+        id: 2,
+        name: 'Lord',
+    },
+    {
+        id: 3,
+        name: 'Laura',
+    },
+]
